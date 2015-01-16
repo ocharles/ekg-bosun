de4b7ce3167e0e28cca7e43deb9cee9ba516bbe4
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 19:35:28 +0000

    Regenerate default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,16 @@
-{ cabal, ekgCore, network, text, vector, wreq }:
-cabal.mkDerivation (self: {
+{ mkDerivation, aeson, base, ekg-core, http-client, lens, network
+, network-uri, old-locale, stdenv, text, time, unordered-containers
+, vector, wreq
+}:
+mkDerivation {
   pname = "ekg-bosun";
-  version = "1.0.0";
+  version = "1.0.3";
   src = ./.;
-  buildDepends = [ ekgCore network text vector wreq ];
-})
+  buildDepends = [
+    aeson base ekg-core http-client lens network network-uri old-locale
+    text time unordered-containers vector wreq
+  ];
+  homepage = "http://github.com/ocharles/ekg-bosun";
+  description = "Send ekg metrics to a Bosun instance";
+  license = stdenv.lib.licenses.bsd3;
+}
